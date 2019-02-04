c00067dbb31e449da6c11652ea9f63ad78801e61
HehexOne <werty.mn2@yandex.ru>
2019-02-04 22:10:30 +0300

    Created and deleted some errors and something

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 import pygame
-import mod_lib
+from mod_lib import *
 
 pygame.init()
-size = width, height = 1280, 720
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(screen_size)
 
 fps = 60
 clock = pygame.time.Clock()
 
-pygame.display.flip()
+dp = DataProvider()
+if dp.get_value("isNew"):
+    generate_map()
+
+tile_group = pygame.sprite.Group()
+water_group = pygame.sprite.Group()
+creatures_group = pygame.sprite.Group()
+
+
+def init():
+    pass
+
+
+def start_screen():
+    pass
+
+
+def loading():
+    pass
+
+
+def game():
+    screen.blit(pygame.transform.scale(load_image("map.png"), (size * tile_size, size * tile_size)), (0, 0))
+    Player(creatures_group, 9, 4, 0, 0)
+
+
+def pause():
+    pass
+
 
 running = True
-
-
-
+game()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     clock.tick(fps)
+    screen.blit(pygame.transform.scale(load_image("map.png"), (size * tile_size, size * tile_size)), (0, 0))
+    creatures_group.draw(screen)
+    creatures_group.update()
+    tile_group.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
--- mod_lib.py
+++ mod_lib.py
@@ -3,78 +3,220 @@ import pygame
 import json
 import math
 from PIL import Image, ImageFilter, ImageEnhance
+import os
 
 fps = 60
+screen_size = width, height = 1600, 900
+tile_size = 100
 
 # High values of this parameter not recommended if you have got not so powerful PC (64 max)
 size = 64  # Size of map (Example: size = 256  =>  map: 256x256)
 
 
-grass = 255
-sand = 150
+# ID's for our map array which goes to save.json (For future comparing)
 water = 0
+grass = 1
+sand = 2
+snow = 3
 
 
 # Min value is 0 and max value is 255
-water_barrier = 70  # 70
-grass_barrier = 170  # 170
+water_barrier = 100  # Standard values: 70
+grass_barrier = 140  # Standard values: 140
 
 
+textures = {
+    0: "water.png",
+    1: "grass.png",
+    2: "sand.png",
+    3: "snow.png"
+}
+
+
+# Reset saves.json
+# Delete all saves
 def reset():
     js = {"isNew": True}
     json.dump(js, open("data/save.json", 'w'))
 
 
-class SeedException(Exception):
-    pass
+# Custom function for load images
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
 
 
+# Function that generates the world and sets "isNew" variable to False
 def generate_map():
+    print("\n\n----GENERATING ON----")
     img = Image.new("RGB", (size, size), (0, 0, 0))
     pixels = img.load()
-    print("Generating noise")
+    print("  Generating noise.")
     for x in range(0, size):
         for y in range(size):
-            val = int((random.uniform(0, 5) * 50) * math.pi / math.sin(random.uniform(0, 10)) * math.cos(math.sqrt(2.1)))
+            # Difficult formula for generation
+            val = int((random.uniform(0, 6.5) * 50) *
+                      math.pi /
+                      math.sin(random.uniform(0, 10)) *
+                      math.cos(math.sqrt(2.1)))
             if val < 100:
                 pixel = 0
             else:
                 pixel = 255
             pixels[x, y] = pixel, 0, 0
-    print("Generated noise!")
+    print("  Generated noise!!")
     img = img.filter(ImageFilter.GaussianBlur(1))
     img = ImageEnhance.Contrast(img).enhance(3)
-    print("Getting enhancement")
+    print(" Getting enhancement")
     pixels = img.load()
-    print("Few seconds...")
+    print("    Few seconds..")
     arr = [[0 for j in range(size)] for i in range(size)]
+    snow_enabled = random.choice([0, 1])
     for x in range(0, size):
         for y in range(size):
             if grass_barrier < pixels[x, y][0]:
-                arr[x][y] = 255
-                pixels[x, y] = 0, 160, 0
+                arr[x][y] = grass
+                pixels[x, y] = 0, random.randint(60, 160), 0
             elif pixels[x, y][0] < water_barrier:
-                arr[x][y] = 0
-                pixels[x, y] = 0, 0, 160
+                arr[x][y] = water
+                pixels[x, y] = 30, 30, random.randint(60, 180)
             else:
-                arr[x][y] = 150
-                pixels[x, y] = 255, 211, 95
-    img.save('map.png')
+                arr[x][y] = sand
+                pixels[x, y] = random.randint(210, 240),\
+                               random.randint(160, 211),\
+                               random.randint(40, 95)
+            if arr[x][y] != water and arr[x][y] != sand and\
+                    math.degrees(random.uniform(0, 40) // 14)\
+                    // 10 == snow_enabled:
+                arr[x][y] = snow
+                pixels[x, y] = random.randint(220, 255),\
+                               random.randint(220, 255), 255
+    img.save('data/map.png')
     img.close()
     js = json.load(open("data/save.json", 'r'))
     js["map"] = arr
     js["isNew"] = False
     json.dump(js, open("data/save.json", 'w'))
-    print("----GENERATED MAP----")
+    print("----GENERATED MAP----\n\n")
 
 
-class Creature:
+class DataProvider:
 
-    def __init__(self, name, hp, damage):
+    def __init__(self):
+        self.data = json.load(open("data/save.json", 'r'))
+
+    def get_value(self, name):
+        return self.data.get(name, None)
+
+    def set_value(self, name, val):
+        self.data[name] = val
+
+    def save(self):
+        json.dump(self.data, open("data/save.json", 'w'))
+
+
+class Creature(pygame.sprite.Sprite):
+
+    def __init__(self, group, name, hp, damage, damage_delta):
+        super().__init__(group)
         self.name = name
         self.hp = hp
         self.max_hp = hp
         self.damage = damage
-        self.get_damage = lambda: random.randint(1, self.damage)
+        self.damage_delta = damage_delta
+        self.get_damage = lambda: random.randint(self.damage - self.damage_delta,
+                                                 self.damage)
         self.coins = 0
+        self.speed = 20
+        # self.image = load_image(image)
+        # self.rect = self.image.get_rect()
+        # self.rect.x = 0
+        # self.rect.y = 0
+        self.side = 0
 
+    def cast_damage(self, amount):
+        self.hp -= amount
+        if self.hp <= 0:
+            self.kill()
+            del self
+
+
+class Player(Creature):
+
+    def __init__(self, group, columns, rows, x, y):
+        super().__init__(group, "Player", 10, 10, 9)
+        self.frames = []
+        self.cut_sheet(load_image('player.png'), columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def restore_from_save(self, d):
+        self.name = d["name"]
+        self.hp = d["hp"]
+        self.max_hp = d["max_hp"]
+        self.damage = d["damage"]
+        self.damage_delta = d["damage_delta"]
+        self.get_damage = lambda: random.randint(self.damage - self.damage_delta,
+                                                 self.damage)
+        self.coins = d["coins"]
+        self.rect.x = d["coords"]["x"]
+        self.rect.y = d["coords"]["y"]
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+    def generate_save(self):
+        return {"name": self.name,
+                "hp": self.hp,
+                "max_hp": self.max_hp,
+                "damage": self.damage,
+                "damage_delta": self.damage_delta,
+                "coins": self.coins,
+                "coords": {"x": self.rect.x,
+                           "y": self.rect.y}}
+
+
+class Tile(pygame.sprite.Sprite):
+
+    def __init__(self, group, kind, x, y):
+        super().__init__(group)
+        self.image = load_image(textures[kind])
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Camera:
+
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
