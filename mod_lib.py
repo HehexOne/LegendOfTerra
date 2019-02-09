7e070ca4a96879eed2776b782d1b9f2b1ab02df7
HehexOne <werty.mn2@yandex.ru>
2019-02-09 18:11:29 +0400

    New generator structure

--- mod_lib.py
+++ mod_lib.py
@@ -3,15 +3,16 @@ import pygame
 import json
 import math
 from PIL import Image, ImageFilter, ImageEnhance
+import numpy as np
 import os
 
 fps = 60
-screen_size = width, height = 1600, 900
-tile_size = 100
+screen_size = width, height = 900, 900
+tile_size = 225
 
-# High values of this parameter not recommended if you have got not so powerful PC (64 max)
+# High values of this parameter
+#  Not recommended if you have got not so powerful PC (64 max)
 size = 64  # Size of map (Example: size = 256  =>  map: 256x256)
-
 
 # ID's for our map array which goes to save.json (For future comparing)
 water = 0
@@ -19,11 +20,9 @@ grass = 1
 sand = 2
 snow = 3
 
-
 # Min value is 0 and max value is 255
 water_barrier = 100  # Standard values: 70
 grass_barrier = 140  # Standard values: 140
-
 
 textures = {
     0: "water.png",
@@ -56,6 +55,11 @@ def load_image(name, colorkey=None):
     return image
 
 
+def set_val(x, y, val, arr):
+    tmp_val = width // tile_size
+    arr[x // tmp_val][y // tmp_val][y % tmp_val][x % tmp_val] = val
+
+
 # Function that generates the world and sets "isNew" variable to False
 def generate_map():
     print("\n\n----GENERATING ON----")
@@ -80,31 +84,36 @@ def generate_map():
     print(" Getting enhancement")
     pixels = img.load()
     print("    Few seconds..")
-    arr = [[0 for j in range(size)] for i in range(size)]
-    snow_enabled = random.choice([0, 1])
-    for x in range(0, size):
+    tmp_val = width // tile_size
+    arr = np.zeros((size // tmp_val, size // tmp_val, tmp_val, tmp_val),
+                   dtype=np.int32)
+    snow_disabled = random.choice([0, 1])
+    for x in range(size):
         for y in range(size):
             if grass_barrier < pixels[x, y][0]:
-                arr[x][y] = grass
+                set_val(x, y, grass, arr)
                 pixels[x, y] = 0, random.randint(60, 160), 0
             elif pixels[x, y][0] < water_barrier:
-                arr[x][y] = water
+                set_val(x, y, water, arr)
                 pixels[x, y] = 30, 30, random.randint(60, 180)
             else:
-                arr[x][y] = sand
-                pixels[x, y] = random.randint(210, 240),\
-                               random.randint(160, 211),\
+                set_val(x, y, sand, arr)
+                pixels[x, y] = random.randint(210, 240), \
+                               random.randint(160, 211), \
                                random.randint(40, 95)
-            if arr[x][y] != water and arr[x][y] != sand and\
-                    math.degrees(random.uniform(0, 40) // 14)\
-                    // 10 == snow_enabled:
-                arr[x][y] = snow
-                pixels[x, y] = random.randint(220, 255),\
+            if arr[x // tmp_val][y // tmp_val][x % tmp_val][y % tmp_val]\
+                    != water and arr[x // tmp_val][y // tmp_val]\
+                [x % tmp_val][y % tmp_val] != sand and \
+                    math.degrees(random.uniform(0, 40) // 14) \
+                    // 10 == snow_disabled:
+                arr[x // tmp_val][y // tmp_val][x % tmp_val][y % tmp_val]\
+                    = snow
+                pixels[x, y] = random.randint(220, 255), \
                                random.randint(220, 255), 255
-    img.save('data/map.png')
+    img.save("data/map.png")
     img.close()
     js = json.load(open("data/save.json", 'r'))
-    js["map"] = arr
+    js["map"] = arr.tolist()
     js["isNew"] = False
     json.dump(js, open("data/save.json", 'w'))
     print("----GENERATED MAP----\n\n")
@@ -134,8 +143,9 @@ class Creature(pygame.sprite.Sprite):
         self.max_hp = hp
         self.damage = damage
         self.damage_delta = damage_delta
-        self.get_damage = lambda: random.randint(self.damage - self.damage_delta,
-                                                 self.damage)
+        self.get_damage = lambda: random.randint(
+            self.damage - self.damage_delta,
+            self.damage)
         self.coins = 0
         self.speed = 20
         # self.image = load_image(image)
@@ -167,8 +177,9 @@ class Player(Creature):
         self.max_hp = d["max_hp"]
         self.damage = d["damage"]
         self.damage_delta = d["damage_delta"]
-        self.get_damage = lambda: random.randint(self.damage - self.damage_delta,
-                                                 self.damage)
+        self.get_damage = lambda: random.randint(
+            self.damage - self.damage_delta,
+            self.damage)
         self.coins = d["coins"]
         self.rect.x = d["coords"]["x"]
         self.rect.y = d["coords"]["y"]
@@ -206,17 +217,3 @@ class Tile(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
-
-class Camera:
-
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-# gn = threading.Thread(target=generate_map)
-# gn.start()
-# print("hello, world!")
-# while gn.is_alive():
-#     pass
-# print("Ended thread 1")
+
